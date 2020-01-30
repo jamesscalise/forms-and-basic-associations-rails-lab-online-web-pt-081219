9e4909913a6f8cae140acfa49bdde943bf324bf9
James Scalise <jamescscalise@gmail.com>
2020-01-30 20:35:36 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,10 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name: name)
+  end
+  
+  def artist_name
+    self.artist.name
   end
 end
